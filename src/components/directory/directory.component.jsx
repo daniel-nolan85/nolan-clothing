7e4ecc3844f2ab4920c88af32e263c2d94c9b235
Daniel Nolan <daniel@nolancode.com>
2021-01-22 08:26:18 +0000

    moved directory and shop data into own reducer

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,60 +1,23 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+
+import {selectDirectorySections} from '../../redux/directory/directory.selectors';
 
 import MenuItem from '../menu-item/menu-item.component';
 
 import './directory.styles.scss';
 
-class Directory extends React.Component {
-    constructor() {
-        super();
+const Directory = ({sections}) => (
+    <div className="directory-menu">
+        {sections.map(({id, ...otherSectionProps}) => (
+            <MenuItem key={id} {...otherSectionProps} />
+        ))}
+    </div>
+);
 
-        this.state = {
-            sections: [
-                {
-                  title: 'hats',
-                  imageUrl: 'https://cdn.pixabay.com/photo/2017/05/13/12/40/fashion-2309519_960_720.jpg',  //Image by <a href="https://pixabay.com/users/1643606-1643606/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2309519">Pera Detlic</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2309519">Pixabay</a>
-                  id: 1,
-                  linkUrl: 'hats'
-                },
-                {
-                  title: 'jackets',
-                  imageUrl: 'https://cdn.pixabay.com/photo/2017/08/05/15/04/leather-2584298_960_720.jpg',  //Image by <a href="https://pixabay.com/users/stocksnap-894430/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2584298">StockSnap</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2584298">Pixabay</a>
-                  id: 2,
-                  linkUrl: ''
-                },
-                {
-                  title: 'trainers',
-                  imageUrl: 'https://images.unsplash.com/photo-1495555961986-6d4c1ecb7be3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjN8fHNuZWFrZXIlMjBwaG90b3Nob290fGVufDB8fDB8&auto=format&fit=crop&w=500&q=60',  //<span>Photo by <a href="https://unsplash.com/@hermez777?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Hermes Rivera</a> on <a href="https://unsplash.com/s/photos/sneaker-photoshoot?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
-                  id: 3,
-                  linkUrl: ''
-                },
-                {
-                  title: 'womens',
-                  imageUrl: 'https://cdn.pixabay.com/photo/2017/09/01/21/53/blue-2705642_960_720.jpg',  //Image by <a href="https://pixabay.com/users/danielsampaioneto-5723932/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2705642">Daniel Sampaio Donate if you want (Paypal)</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2705642">Pixabay</a>
-                  size: 'large',
-                  id: 4,
-                  linkUrl: ''
-                },
-                {
-                  title: 'mens',
-                  imageUrl: 'https://images.unsplash.com/photo-1600442715817-4d9c8b6c729f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8&auto=format&fit=crop&w=500&q=60',  //<span>Photo by <a href="https://unsplash.com/@karsten116?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Karsten Winegeart</a> on <a href="https://unsplash.com/s/photos/black-man-cool?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
-                  size: 'large',
-                  id: 5,
-                  linkUrl: ''
-                }
-            ]
-        }
-    }
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
 
-    render() {
-        return (
-            <div className="directory-menu">
-                {this.state.sections.map(({id, ...otherSectionProps}) => (
-                    <MenuItem key={id} {...otherSectionProps} />
-                ))}
-            </div>
-        );
-    }
-}
-
-export default Directory;
+export default connect(mapStateToProps)(Directory);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,23 @@
 import {combineReducers} from 'redux';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import basketReducer from './basket/basket.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['basket']
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
-    basket: basketReducer
+    basket: basketReducer,
+    directory: directoryReducer,
+    shop: shopReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
