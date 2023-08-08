735449f1c11892487e0c3ec578127e1b14879703
daniel-nolan85 <danielnolan85@yahoo.com>
2023-08-08 12:03:13 -0700

    updated modal to redirect users

--- src/components/modal/modal.component.jsx
+++ src/components/modal/modal.component.jsx
@@ -1,37 +1,35 @@
 import React from 'react';
-
+import { useHistory } from 'react-router-dom';
 import Modal from 'react-modal';
 
 import './modal.styles.scss';
 
-Modal.setAppElement('#root')
+Modal.setAppElement('#root');
 const MyModal = () => {
-    const [modalIsOpen, setmodalIsOpen] = React.useState(true)
-    return (
-        <div>
-            {/* <button onClick={() => setmodalIsOpen(true)}>Open modal</button> */}
-            <Modal 
-                className='my-modal' 
-                isOpen={modalIsOpen} 
-                onRequestClose={() => setmodalIsOpen(false)}
-                // style= {
-                //     {
-                //         overlay: {
-                //             backgroundColor: 'grey'
-                //         },
-                //         content: {
-                //             backgroundColor: '#fff;'
-                //         }
-                //     }
-                // }
-            >
-                <h2>This is a portfolio project</h2>
-                <p>All items are contained for display purposes only</p>
-                <button className='close' onClick={() => setmodalIsOpen(false)}>Got it</button>
-            </Modal>
-            
-        </div>
-    )
+  const [modalIsOpen, setmodalIsOpen] = React.useState(true);
+
+  const history = useHistory(); // Initialize useHistory
+
+  const closeModal = () => {
+    setmodalIsOpen(false);
+    history.push('/'); // Redirect to homepage after closing modal
+  };
+
+  return (
+    <div>
+      <Modal
+        className='my-modal'
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+      >
+        <h2>This is a portfolio project</h2>
+        <p>All items are contained for display purposes only</p>
+        <button className='close' onClick={closeModal}>
+          Got it
+        </button>
+      </Modal>
+    </div>
+  );
 };
 
 export default MyModal;
